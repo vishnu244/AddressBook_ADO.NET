198ff8173a1ce092aa22fba60f7011c4f47e2c00
vishnu244 <vishnunali244@gmail.com>
2022-06-13 13:02:19 +0530

    [Vishnu]-created AddressBook Database

---
+++ AddressBook_ADO.NET/AddressBookData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_ADO.NET
+{
+    public class AddressBookData
+    {
+        public  void Create_Database()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=LAPTOP-7SFIPVKT; Initial Catalog =master; Integrated Security = True;");
+                Connection.Open();
+                SqlCommand command = new SqlCommand("Create database AddressbookForADO;", Connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("AddressbookService Database created successfully!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating database:" + e.Message + "\t");
+            }
+        }
+    }
+}
--- AddressBook_ADO.NET/Program.cs
+++ AddressBook_ADO.NET/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook_ADO.NET;
+
 Console.WriteLine("Welcomw to AddressBook ADO.NET!");
+
+AddressBookData addressBookData = new AddressBookData();
+addressBookData.Create_Database();
